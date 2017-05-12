8c27012d64cab225b92630dfb12ac302bf6c6c0f
Maciej Górski <maciek.gorski@gmail.com>
2017-05-12 09:55:24 +0200

    Provider does not accept nullable.

--- app/src/main/java/io/userfeeds/infrastructure/common/Provider.kt
+++ app/src/main/java/io/userfeeds/infrastructure/common/Provider.kt
@@ -4,13 +4,10 @@ import kotlin.LazyThreadSafetyMode.NONE
 
 typealias Initializer<T> = () -> T
 
-abstract class Provider<T>(initializer: Initializer<T>) {
+abstract class Provider<T : Any>(initializer: Initializer<T>) {
 
     private val value by lazy(NONE, initializer)
     var override: Initializer<T>? = null
 
-    fun get(): T {
-        val local = override
-        return if (local != null) local.invoke() else value
-    }
+    fun get() = override?.invoke() ?: value
 }
