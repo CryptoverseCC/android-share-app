ec0e6f64f983eab3f9650d6bdb573a453207ce85
Maciej Górski <maciek.gorski@gmail.com>
2017-05-22 16:04:32 +0200

    Retrieve identity from web and display it in share app.

--- app/src/main/java/io/userfeeds/share/MainActivity.kt
+++ app/src/main/java/io/userfeeds/share/MainActivity.kt
@@ -1,12 +1,28 @@
 package io.userfeeds.share
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.main_activity.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        importIdentity.setOnClickListener {
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (scanResult != null) {
+            debugView.text = scanResult.contents
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
